48cea95a25d42037a69ddd8c39b7e063bca0078d
xht <xht@163.com>
2019-02-13 11:08:58 +0800

    更新

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from main.models import JdFirstCate
+from search.models import JdBrand
+from shop.models import JdShop
 
 
 def index(request):
@@ -13,8 +15,11 @@ def index(request):
         for second_cate in first_cate.second_cates:
             third_cate_list = second_cate.jdthirdcate_set.all()
             second_cate.third_cates = third_cate_list
-
+    brand_list=JdBrand.objects.all()
+    shop_list=JdShop.objects.all()
     context = {
         'first_cates': first_cates,
+        'brand_list':brand_list,
+        'shop_list':shop_list,
     }
     return render(request, 'index.html', context)
