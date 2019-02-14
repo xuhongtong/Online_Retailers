3627adb5151b8d6376e3bb7302e30051b82989b4
YKCCC <17879511759@163.com>
2019-02-14 09:47:07 +0800

    商品展示,主页index.html的修改以及main.views的修改

--- apps/main/views.py
+++ apps/main/views.py
@@ -17,13 +17,18 @@ def index(request):
         for second_cate in first_cate.second_cates:
             third_cate_list = second_cate.jdthirdcate_set.all()
             second_cate.third_cates = third_cate_list
-
-    brand_list=JdBrand.objects.all()
-    shop_list=JdShop.objects.all()
-
+    # 获取商品数据
+    shop_list = JdShop.objects.all()
+    brand_list = JdBrand.objects.all()
+    shops = shop_list.filter(id__lt=7)
+    brands = brand_list.filter(brand_id__lt=4)
+    shop1 = shop_list.filter(brand_name='Apple', id__lt=6)
     context = {
         'first_cates': first_cates,
         'brand_list':brand_list,
         'shop_list':shop_list,
+        'brands': brands,
+        'shops': shops,
+        'shop1':shop1,
     }
     return render(request, 'index.html', context)
