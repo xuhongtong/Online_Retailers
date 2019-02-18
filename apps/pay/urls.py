5f63162217484ec8c4edb7cae8f0d0cb569578ff
yinyaowhy <yinyaowhy@gmail.com>
2019-02-18 21:10:39 +0800

    添加支付成功回调,未完成

--- apps/pay/urls.py
+++ apps/pay/urls.py
@@ -6,6 +6,6 @@ from pay import views
 
 urlpatterns = [
     url(r'^alipay/',views.pay_view,name='pay'),
-    url(r'^order_status/',views.order_status_view,name='order_status'),
+    url(r'^success/',views.success_view,name='success'),
     url(r'^callback/',views.aliapy_back_url),
 ]
